a1c8aef1b5fc3910e29aea5dafbd8850734d1ea8
Helga13 <oliatrubina@yandex.ru>
2017-04-28 20:52:17 +0300

    first commit

---
+++ prod/js/app.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+	
+	$('.sidebar__item__head').on('click', function(){
+//		$(this).toggleClass('visible').parent().siblings().children('.sidebar__item__head').removeClass('visible');
+		
+//		var this_ = $(this),
+//        parent = this_.parents('.sidebar__item'),
+//        blockThis = parent.find('.sidebar_list'),
+//        accord = $('.sidebar__item'),
+//        block = accord.find('.sidebar_list');
+//
+//        if (!parent.hasClass('is-active')) {
+//            accord.stop(true, true).removeClass('is-active');
+//            block.stop(true, true).slideUp(400);
+//            parent.stop(true, true).addClass('is-active');
+//            blockThis.stop(true, true).slideDown(400);
+//        } else {
+//            parent.stop(true, true).removeClass('is-active');
+//            blockThis.stop(true, true).slideUp(400);
+//        }
+//        return false;
+	});
+	
+	
+})
